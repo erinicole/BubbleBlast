20847564faca9a527c65f2beddcdb6f3351453cb
kristinamiller <kfm287@gmail.com>
2019-09-23 10:28:05 -0700

    added mongo

--- app.js
+++ app.js
@@ -3,10 +3,27 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose.connect(db, {
+  useNewUrlParser: true
+}).then((test) => {
+  console.log("Connected to mongoDB");
+  console.log(test);
+}).catch((err) => {
+  console.log(err);
+});
 
 app.get("/", (req, res) => {
+
   res.send("Hello a/A!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets );
 
 const port = process.env.PORT || 5000;
 
