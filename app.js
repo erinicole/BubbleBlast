058f6231b9580125a4d45ca7081266298163c540
App Academy Student <student@appacademy.io>
2019-09-24 12:19:16 -0700

    added MERN stuff

--- app.js
+++ app.js
@@ -5,29 +5,35 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
+const questions = require("./routes/api/questions");
+const path = require("path");
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+//L
+
 
 mongoose.connect(db, {
   useNewUrlParser: true
-}).then((test) => {
+}).then(() => {
   console.log("Connected to mongoDB");
-  console.log(test);
 }).catch((err) => {
   console.log(err);
 });
 
 app.get("/", (req, res) => {
-
   res.send("Hello a/A!");
 });
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets );
+app.use("/api/questions", questions);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
