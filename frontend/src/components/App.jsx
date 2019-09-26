0b4a80b890a37f8338c198ca8447cc815c336836
Kristina Miller <kristinamiller@Kristinas-MacBook-Air.local>
2019-09-26 11:25:02 -0700

    styling for pages 1, 2, 4

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container'
 import HomePageContainer from "./home_page/home_page_container";
+import QuestionPageContainer from "./question_page/question_page_container";
+import ResultPageContainer from "./result_page/result_page_container";
+import Footer from './greeting/footer'
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -8,8 +12,14 @@ function App() {
       <header className="test">
         <GreetingContainer />
       </header>
-      
-      <HomePageContainer />
+      <Switch>
+        <Route exact path="/question" component={QuestionPageContainer} />
+        <Route exact path="/results" component={ResultPageContainer} />
+        <Route exact path="/" component={HomePageContainer}/>
+      </Switch>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- frontend/src/components/question_page/question_page.jsx
+++ frontend/src/components/question_page/question_page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class QuestionPage extends React.Component {
 
@@ -12,8 +12,30 @@ class QuestionPage extends React.Component {
   render() {
 
     return (
-      <div>
-        This is the Question Page
+      
+        <div class="question-main-section">
+          <div class="question-div">963 - 643 = ___?</div>
+          <div>
+            <ul class="answers-ul">
+              <li class="answers-li">
+                <div class="answer-icon">A</div>
+                <div class="answer-body">420</div>
+              </li>
+              <li class="answers-li">
+                <div class="answer-icon">B</div>
+                <div class="answer-body">320</div>
+              </li>
+              <li class="answers-li">
+                <div class="answer-icon">C</div>
+                <div class="answer-body">329</div>
+              </li>
+              <li class="answers-li">
+                <div class="answer-icon">D</div>
+                <div class="answer-body">310</div>
+              </li>
+
+            </ul>
+          </div>
       </div>
     )
   }
